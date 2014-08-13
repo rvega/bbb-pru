67f7e7f7c8f49aec16444a70877998135f885645
Rafael Vega <email.rafa@gmail.com>
2014-08-13 04:10:59 +0000

    Added gitignore and a little delay in pru code

--- app-c/pru.c
+++ app-c/pru.c
@@ -1,6 +1,12 @@
 volatile register unsigned int __R31;
 
 int main(int argc, const char *argv[]){
+   // Loop a bit to make time
+   int i, j;
+   for(i=0; i<0x00a00000; i++){
+      for(j=0; j<5; j++);
+   }
+
    // Send notification to Host for program completion
    __R31 = 35;
    __halt(); 
