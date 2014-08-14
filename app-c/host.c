729a09e52482cbc8f4e9519a63ec504a05a88280
Rafael Vega <email.rafa@gmail.com>
2014-08-14 22:14:14 +0000

    Figured out interrupts and shared ram

--- app-c/host.c
+++ app-c/host.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
 #include <prussdrv.h>
 #include <pruss_intc_mapping.h>
@@ -10,8 +14,36 @@
 #error "START_ADDR must be defined"
 #endif
 
-int main(int argc, const char *argv[]){
-   printf("Starting \n");
+void load_device_tree_overlay(){
+   // Check if device tree overlay is loaded, load if needed.
+   int device_tree_overlay_loaded = 0; 
+   FILE* f;
+   f = fopen("/sys/devices/bone_capemgr.9/slots","rt");
+   if(f==NULL){
+      printf("Initialisation failed (fopen rt)");
+      exit(1);
+   }
+   char line[256];
+   while(fgets(line, 256, f) != NULL){
+      if(strstr(line, "PRU-DTO") != NULL){
+         device_tree_overlay_loaded = 1; 
+      }
+   }
+   fclose(f);
+
+   if(!device_tree_overlay_loaded){
+      f = fopen("/sys/devices/bone_capemgr.9/slots","w");
+      if(f==NULL){
+         printf("Initialisation failed (fopen)");
+         exit(1);
+      }
+      fprintf(f, "PRU-DTO");
+      fclose(f);
+      usleep(100000);
+   }
+}
+
+void init_pru_program(){
    tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
    prussdrv_init();
    prussdrv_open(PRU_EVTOUT_0);
@@ -19,12 +51,47 @@ int main(int argc, const char *argv[]){
 
    prussdrv_load_datafile(PRU_NUM, "./data.bin");
    prussdrv_exec_program_at(PRU_NUM, "./text.bin", START_ADDR);
+}
 
-   printf("Waiting\n");
-   prussdrv_pru_wait_event(PRU_EVTOUT_0);
-   prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+int finish = 0;
+void signal_handler(int signal){
+   finish = 1;
+}
 
-   printf("Done\n");
+int main(int argc, const char *argv[]){
+   printf("\n\n");
+
+   // Listen to SIGINT signals (program termination)
+   signal(SIGINT, signal_handler);
+
+   // Load device tree overlay to enable PRU hardware.
+   load_device_tree_overlay();
+
+   // Load and run binary into pru0
+   init_pru_program();
+
+   // Get pointer to shared ram
+   void* p;
+   prussdrv_map_prumem(PRUSS0_SHARED_DATARAM, &p);
+   unsigned int* shared_ram = (unsigned int*)p;
+
+   while(!finish){
+      // Wait for interrupt from PRU
+      prussdrv_pru_wait_event(PRU_EVTOUT_0);
+      prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+
+      // Read number in shared ram
+      printf("Got interrupt: %u \n", shared_ram[0]);
+
+      if(shared_ram[0]==5){
+         // Write number in shared ram and send interrupt request
+         // to pru0
+         shared_ram[1]=2; 
+         prussdrv_pru_send_event(ARM_PRU0_INTERRUPT);
+      }
+   }
+
+   printf("Disabling PRU.\n");
    prussdrv_pru_disable(PRU_NUM);
    prussdrv_exit ();
    return 0;
--- app-c/pru.c
+++ app-c/pru.c
@@ -1,14 +1,51 @@
+// Macro for accessing a hardware register (32 bit)
+#define HWREG(x) (*((volatile unsigned int *)(x)))
+
+
 volatile register unsigned int __R31;
 
 int main(int argc, const char *argv[]){
-   // Loop a bit to make time
-   int i, j;
-   for(i=0; i<0x00a00000; i++){
-      for(j=0; j<5; j++);
+   // Shared ram is at address 0x10000.
+   // See table 4.7 Local Memory Map in page 204 of manual
+   unsigned int* shared_ram = (unsigned int *)0x10000;
+
+   int i;
+   unsigned char done = 0;
+   unsigned int count = 0;
+   while(count<10){
+      // Delay
+      for(i=0; i<10000000; i++);
+
+      // Writing to register 31 sends interrupt requests to the 
+      // ARM system. See section 4.4.1.2.2 Event Interface
+      // Mapping (R31): PRU System Events in page 209 of manual. 
+      // Not sure why vector output is 4 in this case but this is
+      // what they do in
+      __R31 = 35;
+
+      shared_ram[0] = count;
+      count ++;
+
+      // If bit 30 of register 31 is set. That means someone sent 
+      // an interrupt request to this PRU.
+      if(__R31 & (1 << 30)){
+         // Writing to the SICR register clears the interrupt 
+         // request. The address of the register is 0x20024:
+         // 0x20000 is the base address for INTC (interrupt
+         // controller) registers (page 204) and 0x24 is the
+         // offset for the SICR register (page 322, Table 4-102.
+         // PRU_ICSS_INTC Registers. 
+         // The value 21 is the event number sent from the ARM 
+         // (linux) code. Again, not sure why they chose this 
+         // number but that's what they do in other examples.
+         HWREG(0x20024) = 21;
+         count = shared_ram[1];
+      }
    }
 
-   // Send notification to Host for program completion
-   __R31 = 35;
+
+   // stop pru processing
    __halt(); 
+
    return 0;
 }
